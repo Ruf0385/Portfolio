6541005c88a1358eed2288232b7642870317d012
ruf0385 <ruf0385@gmail.com>
2022-04-21 08:27:33 -0400

    big changes

---
+++ build/precache-manifest.1f3a34b96235b9af6d98638bde92f98a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0d06cc66aa07709d611ef767a27f37d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c09463274ae18884214d",
+    "url": "/static/css/main.91a6e9d6.chunk.css"
+  },
+  {
+    "revision": "3c6de7a00f5a15d1de80",
+    "url": "/static/js/2.9c01a9f4.chunk.js"
+  },
+  {
+    "revision": "c09463274ae18884214d",
+    "url": "/static/js/main.91183838.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
